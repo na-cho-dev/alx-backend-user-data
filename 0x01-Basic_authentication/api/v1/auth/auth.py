3d9a4d981a2a3cc697aba41e4684a7a7709cb4a7
Fortune Iheanacho <nachodev369@gmail.com>
2024-11-11 23:00:22 +0100

    Task 3. Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Auth Class Module
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """
+    Manage the API authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Returns False - path and excluded_paths
+        """
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Returns None - request will be the Flask request object
+        """
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Returns None - request will be the Flask request object
+        """
