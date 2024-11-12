c6f90355db87297ebb3a75fc18381791c36c9adf
Fortune Iheanacho <nachodev369@gmail.com>
2024-11-12 23:38:34 +0100

    Task 7. Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,3 +11,19 @@ class BasicAuth(Auth):
     """
     Manage Basic Auth API authentication
     """
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        Returns the Base64 part of the Authorization header
+        for a Basic Authentication:
+        """
+        if authorization_header is None:
+            return None
+        
+        if not isinstance(authorization_header, str):
+            return None
+        
+        if not authorization_header.startswith("Basic "):
+            return None
+        
+        return (authorization_header[len("Basic "):])
