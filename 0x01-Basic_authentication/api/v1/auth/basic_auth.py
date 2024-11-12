2ff7aa8af549debae951ff4c10cc5b5e9e048c47
Fortune Iheanacho <nachodev369@gmail.com>
2024-11-13 00:17:21 +0100

    Task 8. Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ Basic Auth Class Module
 from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -23,3 +24,20 @@ class BasicAuth(Auth):
             return None
 
         return (authorization_header[len("Basic "):])
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Returns the decoded value of a Base64 string
+        base64_authorization_header
+        """
+        if (base64_authorization_header is None or
+                not isinstance(base64_authorization_header, str)):
+            return None
+
+        try:
+            decoded = base64.b64decode(
+                base64_authorization_header, validate=True).decode('utf-8')
+            return decoded
+        except (base64.binascii.Error, ValueError):
+            return None
