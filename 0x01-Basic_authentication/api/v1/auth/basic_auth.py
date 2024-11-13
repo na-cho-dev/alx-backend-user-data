0f5c45511d195f667df88202371d2e3606798416
Fortune Iheanacho <nachodev369@gmail.com>
2024-11-13 03:37:29 +0100

    Task Updated

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -91,23 +91,7 @@ class BasicAuth(Auth):
         Retrieves the User instance for a request:
         """
         auth_header = self.authorization_header(request)
-        if not auth_header:
-            return None
-
         b64_auth_token = self.extract_base64_authorization_header(auth_header)
-        if not b64_auth_token:
-            return None
-
         auth_token = self.decode_base64_authorization_header(b64_auth_token)
-        if not auth_token:
-            return None
-
         email, password = self.extract_user_credentials(auth_token)
-        if not email or not password:
-            return None
-
-        user = self.user_object_from_credentials(email, password)
-        if not user:
-            return None
-
-        return user
+        return self.user_object_from_credentials(email, password)
