648885cf7ec7d1fc012b99aea25e857d9a1ec4e2
Fortune Iheanacho <nachodev369@gmail.com>
2024-11-13 03:18:16 +0100

    Task 10. Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,8 +4,9 @@ Basic Auth Class Module
 """
 from flask import request
 from typing import List, TypeVar
-from api.v1.auth.auth import Auth
+from .auth import Auth
 import base64
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -56,3 +57,19 @@ class BasicAuth(Auth):
         password = decoded_base64_authorization_header.split(":")[1]
 
         return (username, password)
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+        Returns the User instance based on his email and password
+        """
+        if isinstance(user_email, str) and isinstance(user_pwd, str):
+            try:
+                users = User.search({'email': user_email})
+            except Exception:
+                return None
+            if len(users) <= 0:
+                return None
+            if users[0].is_valid_password(user_pwd):
+                return users[0]
+        return None
