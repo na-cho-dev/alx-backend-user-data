b738227aea967dc037239bb9929864ff451183e2
Fortune Iheanacho <nachodev369@gmail.com>
2024-11-13 03:28:03 +0100

    Task 13. Require auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@ Auth Class Module
 """
 from flask import request
 from typing import List, TypeVar
+import fnmatch
 
 
 class Auth():
@@ -14,20 +15,14 @@ class Auth():
         """
         Returns False - path and excluded_paths
         """
-        if (path is None):
+        if path is None:
             return True
 
         if excluded_paths is None or not excluded_paths:
             return True
 
-        if not path.endswith('/'):
-            path += '/'
-
         for excluded_path in excluded_paths:
-            if not excluded_path.endswith('/'):
-                excluded_path += '/'
-
-            if path == excluded_path:
+            if fnmatch.fnmatch(path, excluded_path):
                 return False
 
         return True
