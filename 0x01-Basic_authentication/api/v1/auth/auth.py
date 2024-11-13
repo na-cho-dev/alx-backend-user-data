9188e0ac94cdcf2856a27c5cda95eac1d0d6ebe6
Fortune Iheanacho <nachodev369@gmail.com>
2024-11-13 10:02:27 +0100

    Task 0 Edit

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,14 +15,20 @@ class Auth():
         """
         Returns False - path and excluded_paths
         """
-        if path is None:
+        if (path is None):
             return True
 
         if excluded_paths is None or not excluded_paths:
             return True
 
+        if not path.endswith('/'):
+            path += '/'
+
         for excluded_path in excluded_paths:
-            if fnmatch.fnmatch(path, excluded_path):
+            if not excluded_path.endswith('/'):
+                excluded_path += '/'
+
+            if path == excluded_path:
                 return False
 
         return True
