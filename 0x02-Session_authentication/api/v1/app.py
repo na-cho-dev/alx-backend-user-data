5e1970049ccb038e56e0e5f5c87c434c7340e800
Fortune Iheanacho <nachodev369@gmail.com>
2024-11-14 05:04:11 +0100

    Task 5. Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -37,12 +37,15 @@ def before_request():
 
     excluded_path = ['/api/v1/status/',
                      '/api/v1/unauthorized/',
-                     '/api/v1/forbidden/']
+                     '/api/v1/forbidden/',
+                     '/api/v1/auth_session/login/']
+
     if auth.require_auth(request.path, excluded_path):
-        auth_header = auth.authorization_header(request)
-        user = auth.current_user(request)
-        if auth_header is None:
+        if (auth.authorization_header(request) is None and
+                auth.session_cookie(request) is None):
             abort(401)
+
+        user = auth.current_user(request)
         if user is None:
             abort(403)
 
