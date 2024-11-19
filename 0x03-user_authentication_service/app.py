10c785f68af88561ff562d68e7566c87a72455f2
Fortune Iheanacho <nachodev369@gmail.com>
2024-11-19 16:36:53 +0100

    Task Update

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -47,6 +47,9 @@ def login():
         abort(401)
 
     session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 if __name__ == "__main__":
