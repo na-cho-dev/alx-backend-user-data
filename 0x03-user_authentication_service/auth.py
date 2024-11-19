bc8ef3dd97970724f2c62fda9c201e715165d7e1
Fortune Iheanacho <nachodev369@gmail.com>
2024-11-19 14:40:45 +0100

    Task Update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -32,9 +32,12 @@ class Auth:
     """
 
     def __init__(self):
+        """
+        Initialize a new Auth instance
+        """
         self._db = DB()
 
-    def register_user(self, email, password) -> User:
+    def register_user(self, email: str, password: str) -> User:
         """
         Registers a User
         """
