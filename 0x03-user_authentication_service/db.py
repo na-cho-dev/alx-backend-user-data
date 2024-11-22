67125ffcc947755bece53b1d7c1d0a7eb5a5daf4
Fortune Iheanacho <nachodev369@gmail.com>
2024-11-22 09:45:37 +0100

    Task Update

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -41,7 +41,7 @@ class DB:
         """
         Save the user to the database
         """
-        user = User(email, hashed_password)
+        user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
