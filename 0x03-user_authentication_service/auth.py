4e87d3a1928538b6e7b9636e9a2672916d365179
Fortune Iheanacho <nachodev369@gmail.com>
2024-11-22 05:22:16 +0100

    Task 5 Update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -50,9 +50,11 @@ class Auth:
             self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            hashed_password = _hash_password(password).decode('utf-8')
-            user = self._db.add_user(email, hashed_password)
-            return user
+            pass
+
+        hashed_password = _hash_password(password)
+        user = self._db.add_user(email=email, hashed_password=hashed_password)
+        return user
 
     def valid_login(self, email: str, password: str) -> bool:
         """
